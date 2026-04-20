6860e435e4670afe67b3341013d532670d5c532e
Ian <iancaiza123@gmail.com>
2026-04-20 13:34:25 +0200

    commit saltos

--- src/juegos/saltitos/Plataforma.java
+++ src/juegos/saltitos/Plataforma.java
@@ -1,4 +1,20 @@
 package juegos.saltitos;
 
+import java.awt.*;
+
 public class Plataforma {
+    int x;
+    int y;
+    int ancho = 60;
+    int alto = 10;
+
+    public  Plataforma(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.darkGray);
+        g.fillRect(x, y, ancho, alto);
+    }
 }
--- src/juegos/saltitos/Saltitos.java
+++ src/juegos/saltitos/Saltitos.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Saltitos extends JPanel implements ActionListener, KeyListener {
 
@@ -14,14 +16,21 @@ public class Saltitos extends JPanel implements ActionListener, KeyListener {
     private int velocidadY = 0;
     private int gravedad = 1;
     private int velocidadX = 0;
-
+    private ArrayList<Plataforma> plataformas;
+    private Random aleatorio = new Random();
+    private Timer temporizador;
     public Saltitos() {
-        Timer temporizador = new Timer(20, this);
+        temporizador = new Timer(20, this);
         temporizador.start();
 
         this.addKeyListener(this);
         this.setFocusable(true);
         this.setFocusTraversalKeysEnabled(false);
+
+        plataformas = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            plataformas.add(new Plataforma(aleatorio.nextInt(330), i * 70));
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -35,34 +44,64 @@ public class Saltitos extends JPanel implements ActionListener, KeyListener {
 
         g.setColor(Color.darkGray);
         g.fillRect(0, 540, 400, 20);
+
+        for (Plataforma p : plataformas) {
+            p.dibujar(g);
+        }
     }
 
     public void actionPerformed(ActionEvent e) {
         velocidadY += gravedad;
         y += velocidadY;
-
         x += velocidadX;
 
-        if (y > 500) {
-            y = 500;
-            velocidadY = -20;
+
+        if (velocidadY > 0) {
+            for (Plataforma p : plataformas) {
+                if (x + 40 > p.x && x < p.x + p.ancho &&
+                        y + 40 > p.y && y + 40 < p.y + p.alto + velocidadY) {
+                    velocidadY = -20;
+                }
+            }
         }
-        if (x < 0) {
-            x = 0;
+
+
+        if (y < 250) {
+            int diferencia = 250 - y;
+            y = 250;
+
+            for (Plataforma p : plataformas) {
+                p.y += diferencia;
+
+
+                if (p.y > 600) {
+                    p.y = 0;
+                    p.x = aleatorio.nextInt(330);
+                }
+            }
         }
-        if (x > 360) {
-            x = 360;
+
+
+        if (x < 0) x = 0;
+        if (x > 360) x = 360;
+
+
+        if (y > 650) {
+            temporizador.stop();
+            y = 300;
+            velocidadY = 0;
         }
+
         repaint();
     }
 
     public void keyPressed(KeyEvent e) {
         int tecla = e.getKeyCode();
         if (tecla == KeyEvent.VK_LEFT) {
-            x = -7;
+            velocidadX = -7;
         }
         if (tecla == KeyEvent.VK_RIGHT) {
-            x = 7;
+            velocidadX = 7;
         }
     }
 
@@ -70,7 +109,7 @@ public class Saltitos extends JPanel implements ActionListener, KeyListener {
         int tecla = e.getKeyCode();
 
         if (tecla == KeyEvent.VK_LEFT || tecla == KeyEvent.VK_RIGHT) {
-            x = 0;
+            velocidadX = 0;
         }
     }
 
